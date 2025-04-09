77cee26ae5b8ab2191255d2f7e5b9b8b043e9ca3
Thanh Tung <thanhtungeric4@gmail.com>
2025-04-09 10:38:12 +0700

    new code

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'  // 🆕 import thêm Router
+import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/ui">
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter basename="/ui"> {/* 🧭 Đặt đúng base path */}
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 )
