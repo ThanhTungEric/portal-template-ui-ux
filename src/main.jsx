5c15f5585331a500c4b8c31c43491460317945bd
Thanh Tung <thanhtungeric4@gmail.com>
2025-04-09 10:40:45 +0700

    agin

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'  // 🆕 import thêm Router
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/ui"> {/* 🧭 Đặt đúng base path */}
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
